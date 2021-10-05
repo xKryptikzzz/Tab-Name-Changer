e62ecab7ebd5a64cc89b57d4544f45ba4f4cfec2
xKryptikzzz <90012585+xKryptikzzz@users.noreply.github.com>
2021-10-05 10:07:51 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,3 @@
-alert("what would you like to name the tab") var title
-if (title != null) {
-document.title = (title)}
+var title = prompt("title name here");
+if (title != null);
+{document.title = title}; void 0;
