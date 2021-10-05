1660a78d3c1b6ebd518aec0a405dc2744880d51d
xKryptikzzz <90012585+xKryptikzzz@users.noreply.github.com>
2021-10-05 08:22:34 -0600

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:alert("what would you like to name the tab")var title if (title != null){document.title = (title); void 0;
+javascript:var title = prompt("title name here"); if (title != null); {document.title = title}; void 0;
